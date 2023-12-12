692355db40c3a00c0738f9813debe5ba1d6497b4
Shaan860 <31506692+Shaan860@users.noreply.github.com>
2023-12-12 20:07:56 +0000

    learn

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,15 @@
+let score = "sandy";
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+let valueInString = String(score);
+console.log(valueInNumber);
+console.log(typeof valueInString);
+
+// "33" => 33
+// "33abc" => NaN , Not a Number
+// true => 1, false => 0
+const magicNum = Math.random();
+console.log(magicNum);
---
+++ 01_basics/mylogics_js.js
@@ -0,0 +1,7 @@
+const myMagicArray = (Math.random() * 10  + 1);
+//console.log(myMagicArray.toFixed(2));
+const myDate = new Date();
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
